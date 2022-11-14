aa5c7507700447c52f00c8025f040ce20ba0f5a0
CuongNguyen <bosscuong.nguyen.22.02.97@gmail.com>
2022-11-14 15:11:48 +0700

    Updates

---
+++ precache-manifest.a521088ac1b8441280225c228ed51dde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bcc44dab45e5a2cd065579fea19dc2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36b7ecd359764dc606f9",
+    "url": "./static/css/main.8a15d8e6.chunk.css"
+  },
+  {
+    "revision": "d397f2b46bef4d33c7ba",
+    "url": "./static/js/2.6535edd6.chunk.js"
+  },
+  {
+    "revision": "36b7ecd359764dc606f9",
+    "url": "./static/js/main.f9e63dc0.chunk.js"
+  },
+  {
+    "revision": "202352f9d752a2415c8a",
+    "url": "./static/js/runtime~main.18399e66.js"
+  }
+]);
